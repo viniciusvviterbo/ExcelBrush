eecec98e2b134df2434651b79ac24b341eb38f40
viniciusvviterbo <viniciusvviterbo@yahoo.com.br>
2020-02-22 11:44:22 -0300

    Alteraçao de bibliotecas usadas e implementaçao completa

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
-# ExcelBrush
+pip3 install argparse
+pip3 install pillow
+pip3 install openpyxl
+
+python3 excel_brush.py -i file_name -s size
--- excel_brush.py
+++ excel_brush.py
@@ -1,51 +1,54 @@
-import sys, getopt
 from PIL import Image
+import argparse
+import openpyxl
 
-def print_instrucoes():
-    print("excel_brush.py -s <size>")
+# Configuracao dos argumentos
+parser = argparse.ArgumentParser(description = 'Um programa para desenhar fotos em planilhas.')
+parser.add_argument('-i', action = 'store', dest = 'image',
+                    default = '', required = True,
+                    help = 'A imagem a ser processada pelo programa.')
+parser.add_argument('-s', action = 'store', dest = 'outputImageSize', required = True,
+                    help = 'O tamanho da imagem a ser retornada')
+
+def rgb2hex(cor):
+    return "{:02x}{:02x}{:02x}".format(cor[0], cor[1], cor[2])
 
 def main():
-    arquivo_imagem = ""
-    tamanho_imagem = 0
 
-    # Recebe as opções e argumentos passados como parâmetros
+    # Recebe os argumentos, se as variaveis nao forem passadas, retorna -h
+    arguments = parser.parse_args()
+
+    imagem_original = Image.open(arguments.image)
+    resolucao_imagem = int(arguments.outputImageSize)
+
     try:
-        opts, args = getopt.getopt(sys.argv,"hi:s:",["help", "image=", "size="])
-
-    # Erro levantado quando opções inseridas não são reconhecidas
-    except getopt.GetoptError:
-        print_instrucoes()
-        sys.exit(1)
-
-    print(sys.argv)
-    print(opts)
-    print(args)
-      
-    for opt, arg in opts:
-        if opt in ("-h", "--help"):
-            print_instrucoes()
-    
-        elif opt in ("-i", "--image"):
-            imagem_original = Image.open(arg)
-        
-        # Identifica a opção informada na lista de argumentos, posição 1
-        elif opt in ("-s", "--size"):
-            # Define o tamanho da resolução como o informado pelo usuário
-            tamanho_imagem = int(arg)
-    
-    try:
+        # Define o nome base dos arquivos a serem criados
+        nome_arquivo = 'andre_' + str(resolucao_imagem) + 'x' + str(resolucao_imagem)
         # Cria uma imagem reduzida que é nada mais que a imagem_original redimensionada com a resolução informada
-        imagem_reduzida = imagem_original.resize((tamanho_imagem, tamanho_imagem), Image.BILINEAR)
+        imagem_reduzida = imagem_original.resize((resolucao_imagem, resolucao_imagem), Image.BILINEAR)
         # Cria a imagem final redimensionando a imagem_reduzida ao tamanho da original
         imagem_pixelada = imagem_reduzida.resize(imagem_original.size, Image.NEAREST)
-        # Define o nome do arquivo recém-criado
-        nome_arquivo = "andre_" + str(tamanho_imagem) + "x" + str(tamanho_imagem) + ".png"
-        # Salva a imagem criada
-        imagem_pixelada.save(nome_arquivo, dpi=(200,200))
+
+        pix = imagem_reduzida.load()
+
+        largura = imagem_reduzida.size[0]
+        altura = imagem_reduzida.size[1]
+
+        wb = openpyxl.Workbook()
+        ws = wb.worksheets[0]
+
+        for x in range(1, largura + 1):
+            ws.row_dimensions[x].height = 18.00 * 1
+            for y in range(1, altura + 1):
+                ws.column_dimensions[ws.cell(x, y).column_letter].width = 2.43 * 1
+                cor = rgb2hex(pix[x - 1, y - 1]).upper()
+                ws.cell(y, x).fill = openpyxl.styles.PatternFill(fgColor=cor, fill_type='solid')
+
+        wb.save(nome_arquivo + '.xlsx')
 
     except():
         print('An error occurred.')
 
 # Chama a função main
-if __name__ == "__main__":
+if __name__ == '__main__':
     main()
